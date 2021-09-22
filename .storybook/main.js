8107068bcd3d0a3f0f706394514f29f949acbb9f
zhangchao <529743595@qq.com>
2021-09-22 17:08:07 +0800

    feat: add typescript support

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -5,6 +7,19 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+    "@storybook/addon-essentials",
+  ],
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.less$/,
+      use: ['style-loader', 'css-loader', 'less-loader'],
+    });
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@src': path.resolve(__dirname, '../src'),
+      '@components': path.resolve(__dirname, '../src/components')
+    }
+    return config
+  }
 }
