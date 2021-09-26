da63170f64644bb20c01e835c4d37356d10c003b
zhangchao <529743595@qq.com>
2021-09-26 14:09:17 +0800

    feat: add more addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,18 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    '@storybook/addon-storysource',
+    '@storybook/addon-actions',
+    '@storybook/addon-viewport',
   ],
-  webpackFinal: async (config, { configType }) => {
+  webpackFinal: async (config, {configType}) => {
     config.module.rules.push({
       test: /\.less$/,
       use: ['style-loader', 'css-loader', 'less-loader'],
@@ -18,8 +21,8 @@ module.exports = {
     config.resolve.alias = {
       ...config.resolve.alias,
       '@src': path.resolve(__dirname, '../src'),
-      '@components': path.resolve(__dirname, '../src/components')
-    }
-    return config
-  }
-}
+      '@components': path.resolve(__dirname, '../src/components'),
+    };
+    return config;
+  },
+};
